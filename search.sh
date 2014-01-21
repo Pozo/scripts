93d72720e965599b930b37a61de96db4d6d5d7a6
Zoltan Polgar <pozo@gmx.com>
2014-01-21 09:49:07 +0100

    changed perl dependency to ruby, mentioned by @bonifaido

--- search.sh
+++ search.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
-#first we need a perl library
-wget -O ~/uri.tar.gz http://search.cpan.org/CPAN/authors/id/G/GA/GAAS/URI-1.60.tar.gz
-tar -xvf ~/uri.tar.gz -C /usr/lib
+#deprecated lines but could be useful elsewhere
+#first we need a perl library 
+#wget -O ~/uri.tar.gz http://search.cpan.org/CPAN/authors/id/G/GA/GAAS/URI-1.60.tar.gz
+#tar -xvf ~/uri.tar.gz -C /usr/lib
 
-value="$(perl -I/usr/lib/URI-1.60 -MURI::Escape -e 'print uri_escape($ARGV[0]);' "$1 stackoverflow" )"
+ruby --version &> /dev/null
+if [ $? -ne 0 ]; then
+	echo no availabe ruby, so we need to install
+	sudo apt-get install ruby
+fi
 
+value="$(ruby -ruri -e 'print URI.escape(ARGV[0])' "$1 stackoverflow" )"
 lynx -accept_all_cookies "http://www.google.com/search?q=$value&btnI=I"
-
-
