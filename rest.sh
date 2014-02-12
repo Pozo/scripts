18a5d3564b72056411718612b71f478034d6a86b
Zoltan Polgar <pozo@gmx.com>
2014-02-12 14:06:51 +0100

    Added info messages

--- rest.sh
+++ rest.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-URL="https://ephubudw0047/ta-angel/skills"
+URL="https://ephubudw0047:8443/ta-angel/skills"
+
+echo "current URL : $URL"
 
 if [ "$1" == "create" ]; then
 	bash rest_create.sh $URL $2
@@ -9,4 +11,6 @@ elif [ "$1" == "update" ]; then
 	bash rest_update.sh $URL $2
 elif [ "$1" == "read" ]; then
 	bash rest_read.sh $URL $2
+else 
+	echo "Only CRUD actions allowed (create,read,update,delete)"
 fi
